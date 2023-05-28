82b244d52c9667179eccfef20785cf60783a15e2
EzraB16 <127159227+EzraB16@users.noreply.github.com>
2023-05-27 22:14:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+name_array=[];
+function submit(){
+    var name1=document.getElementById("name_student_1").value;
+    var name2=document.getElementById("name_student_2").value;
+    var name3=document.getElementById("name_student_3").value;
+    var name_student_4=document.getElementById("name_student_4").value;
+}
